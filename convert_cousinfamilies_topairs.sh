3740b9030bc1a97c3cf2e8354c4fbcc59156e6f0
jjuod <j.juodakis@gmail.com>
2015-11-16 12:23:58 +0100

    now exporting both parent ids, ages, and birthyear

--- convert_cousinfamilies_topairs.sh
+++ convert_cousinfamilies_topairs.sh
@@ -2,23 +2,33 @@
 
 
 ## remove 'next' from the first line if you have no header 
-awk 'NR==1{print "fid" FS "mom1" FS "kid1" FS "mom2" FS "kid2"; next}
-{split($3, m, ","); split($4, k, ",");
+awk 'NR==1{print "fid" FS "mom1" FS "kid1" FS "mom2" FS "kid2"
+	FS "dad1" FS "dad2" FS "birthyear1" FS "birthyear2" FS "dadage1"
+	FS "dadage2" FS "momage1" FS "momage2"; next}
+{split($3, m, ","); split($4, k, ","); split($5, d, ","); split($6, by, ",");
+split($7, da, ","); split($8, ma, ",");
 for(i in k){
 	for(j=i+1; j in k; j++){
 		if(m[i]!=m[j]){
-			print NR-1, m[i], k[i], m[j], k[j]
+			print NR-1, m[i], k[i], m[j], k[j],
+			d[i], d[j], by[i], by[j],
+			da[i], da[j], ma[i], ma[j]
 		}
 	}
 }
 }' input_file_maternal.csv > output_file_maternal.csv
 
-awk 'NR==1{print "fid" FS "dad1" FS "kid1" FS "dad2" FS "kid2"; next}
-{split($3, m, ","); split($4, k, ",");
+awk 'NR==1{print "fid" FS "dad1" FS "kid1" FS "dad2" FS "kid2"
+	FS "mom1" FS "mom2" FS "birthyear1" FS "birthyear2" FS "dadage1"
+	FS "dadage2" FS "momage1" FS "momage2"; next}
+{split($3, d, ","); split($4, k, ","); split($5, m, ","); split($6, by, ",");
+split($7, da, ","); split($8, ma, ",");
 for(i in k){
 	for(j=i+1; j in k; j++){
-		if(m[i]!=m[j]){
-			print NR-1, m[i], k[i], m[j], k[j]
+		if(d[i]!=d[j]){
+			print NR-1, d[i], k[i], d[j], k[j],
+			m[i], m[j], by[i], by[j],
+			da[i], da[j], ma[i], ma[j]
 		}
 	}
 }
--- find_cousins.sql
+++ find_cousins.sql
@@ -1,9 +1,39 @@
 SELECT grfar, grmor,
+	GROUP_CONCAT(moms) as allmoms,
+	GROUP_CONCAT(kids) as allkids,
 	GROUP_CONCAT(dads) as alldads,
-	GROUP_CONCAT(kids) as allkids
+	GROUP_CONCAT(birthyear) as birthyears,
+	GROUP_CONCAT(dadage) as dadages,
+	GROUP_CONCAT(momage) as momages
 FROM (
-	SELECT p1.lpnr_barn as kids, p1.LopNrFar as dads,
-		p2.LopNrFar as grfar, p2.LopNrMor as grmor
+	SELECT p1.lpnr_barn as kids, p1.LopNrMor as moms, p1.LopNrFar as dads,
+		p2.LopNrFar as grfar, p2.LopNrMor as grmor,
+		p1.FoddArBarn as birthyear,
+		p1.FoddArBarn-p1.FoddArFar as dadage,
+		p1.FoddArBarn-p1.FoddArMor as momage
+	FROM parents p1
+	INNER JOIN parents p2
+	ON p2.lpnr_barn=p1.LopNrMor
+	WHERE p1.LopNrMor>0
+) tt WHERE grfar>0 AND grmor>0
+GROUP BY grfar, grmor
+HAVING COUNT(DISTINCT moms)>1
+INTO OUTFILE '/tmp/output.csv' FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';
+
+/* finding cousins whose fathers are fullsibs (brothers) */
+SELECT grfar, grmor,
+	GROUP_CONCAT(dads) as alldads,
+	GROUP_CONCAT(kids) as allkids,
+	GROUP_CONCAT(moms) as allmoms,
+	GROUP_CONCAT(birthyear) as birthyears,
+	GROUP_CONCAT(dadage) as dadages,
+	GROUP_CONCAT(momage) as momages
+FROM (
+	SELECT p1.lpnr_barn as kids, p1.LopNrMor as moms, p1.LopNrFar as dads,
+		p2.LopNrFar as grfar, p2.LopNrMor as grmor,
+		p1.FoddArBarn as birthyear,
+		p1.FoddArBarn-p1.FoddArFar as dadage,
+		p1.FoddArBarn-p1.FoddArMor as momage
 	FROM parents p1
 	INNER JOIN parents p2
 	ON p2.lpnr_barn=p1.LopNrFar
@@ -11,23 +41,6 @@ FROM (
 ) tt WHERE grfar>0 AND grmor>0
 GROUP BY grfar, grmor
 HAVING COUNT(DISTINCT dads)>1
-INTO OUTFILE '/tmp/output_paternal.csv'
-FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
-
-SELECT grfar, grmor,
-        GROUP_CONCAT(moms) as allmoms,
-        GROUP_CONCAT(kids) as allkids
-FROM (
-        SELECT p1.lpnr_barn as kids, p1.LopNrMor as moms,
-                p2.LopNrFar as grfar, p2.LopNrMor as grmor
-        FROM parents p1
-        INNER JOIN parents p2
-        ON p2.lpnr_barn=p1.LopNrMor
-        WHERE p1.LopNrMor>0
-) tt WHERE grfar>0 AND grmor>0
-GROUP BY grfar, grmor
-HAVING COUNT(DISTINCT moms)>1
-INTO OUTFILE '/tmp/output_maternal.csv'
-FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+INTO OUTFILE '/tmp/output.csv' FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';
 
 
